b93112551222b13d8354b8ce943d62b07d3e3518
ajf013 <jeni13franc@gmail.com>
2021-04-02 16:26:00 +0530

    Full code uploaded

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment, Component } from "react";
+import axios from 'axios';
 
 import "./App.css";
 import Banner from "./components/banner/banner.component";
@@ -11,26 +12,35 @@ class App extends Component {
     this.state = {
       searchName: "",
       userData: "",
+      repoData: [],
     };
   }
 
   onInputChange = (event) => {
     this.setState({ searchName: event.target.value }, () => {
       this.getData(this.state.searchName);
+      this.getRepo(this.state.searchName);
     });
   };
+  getRepo(userName) {
+    axios.get(`https://api.github.com/users/${userName}/repos`)
+      .then((res) => {
+        this.setState({ repoData: res.data });
 
+      })
+  }
   getData = (userName) => {
     fetch(`https://api.github.com/users/${userName}`)
       .then((res) => res.json())
       .then((data) => this.setState({ userData: data }))
-      .catch((error) => {
-        alert("Oops! Could not reach GitHub");
-        console.log("Oops! We have an error", error);
-      });
+         .catch((error) => {
+          alert("Oops! Could not reach GitHub");
+          console.log("Oops! We have an error", error);
+        });
   };
 
   render() {
+    console.log(this.state.repoData)
     return (
       <Fragment>
         <Banner />
